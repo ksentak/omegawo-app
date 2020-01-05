f9311d7a93bf91efad368cafe22b79f8cc234b77
Justin Lampe <pierrelampre@gmail.com>
2020-01-05 04:26:52 -0500

    edited files for heroku/mlab

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var cors = require("cors")
 var bodyParser = require("body-parser")
 var app = express()
 var mongoose = require("mongoose")
+const path = require("path");
 var port = process.env.PORT || 5000
 
 app.use(bodyParser.json())
@@ -13,7 +14,7 @@ app.use(
   })
 )
 
-const mongoURI = "mongodb://localhost/mernloginreg"
+const mongoURI = process.env.MONGODB_URI || "mongodb://justin:justin123@ds259878.mlab.com:59878/heroku_fkklvmvz"
 
 mongoose
  .connect(mongoURI, {useNewUrlParser: true})
@@ -23,6 +24,15 @@ mongoose
  var Users = require("./routes/Users")
 
  app.use("/users", Users)
+
+ if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+
+  });
+}
 
  app.listen(port, () => {
     console.log("Server is running on port:" + port)
