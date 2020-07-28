a2b89aeb6e0a7d44e44b906a2f5e98e22457f36b
KSentak <k.sentak@outlook.com>
2020-07-28 13:47:01 -0400

    move from mlab to mongodb atlas

--- server.js
+++ server.js
@@ -1,27 +1,28 @@
-var express = require("express");
-var cors = require("cors");
-var bodyParser = require("body-parser");
+var express = require('express');
+var cors = require('cors');
+var bodyParser = require('body-parser');
 var app = express();
-var mongoose = require("mongoose");
-const path = require("path");
-require("dotenv").config();
+var mongoose = require('mongoose');
+const path = require('path');
+require('dotenv').config();
 var port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 app.use(cors());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/omega-wo";
+const MONGODB_URI = process.env.DB_URI || 'mongodb://localhost/omega-wo';
 
-mongoose.connect(MONGODB_URI, { useNewUrlParser: true })
-  .then(() => console.log("MongoDB Connected!"))
-  .catch(err => console.log(err));
+mongoose
+  .connect(MONGODB_URI, { useNewUrlParser: true })
+  .then(() => console.log('MongoDB Connected!'))
+  .catch((err) => console.log(err));
 
-const Users = require("./routes/Users");
-const savedWorkouts = require("./routes/Workouts");
+const Users = require('./routes/Users');
+const savedWorkouts = require('./routes/Workouts');
 
-app.use("/users", Users);
-app.use("/savedWorkouts", savedWorkouts);
+app.use('/users', Users);
+app.use('/savedWorkouts', savedWorkouts);
 
 if (process.env.NODE_ENV === 'production') {
   // Exprees will serve up production assets
@@ -30,10 +31,10 @@ if (process.env.NODE_ENV === 'production') {
   // Express serve up index.html file if it doesn't recognize route
   const path = require('path');
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client/build/index.html'))
+    res.sendFile(path.resolve(__dirname, 'client/build/index.html'));
   });
-};
+}
 
 app.listen(port, () => {
-  console.log("Server is running on port:" + port)
+  console.log('Server is running on port:' + port);
 });
